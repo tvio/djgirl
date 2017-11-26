fa6a3c45b4689ae341ea25e0cea910ced383b693
tVIO <tvio@centrum.cz>
2017-11-26 14:13:43 +0100

    remove rest2

--- djgirl/settings.py
+++ djgirl/settings.py
@@ -38,11 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-"""
-    'restdemo',
-    'rest_framework_swagger',
-    'rest_framework',
-"""
 ]
 
 
